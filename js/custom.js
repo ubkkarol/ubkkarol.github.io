4a56363555d6f86b9d68c83346e099da44df8c21
Cris <estrada.cristian@hotmail.com>
2024-08-22 20:37:23 +0200

    [*]INDEX Modified according to new Notes, double visual on the mentoring & consulting side

--- js/custom.js
+++ js/custom.js
@@ -26,7 +26,5 @@ var mySwiper = new Swiper('.swiper-container', {
 /*Preloader*/
 $(".preloader-wrap").delay(1500).fadeOut('slow');
 
-function redirectToIndex() {
-  window.location.href = 'visit.html';
-}
+
 
